1108e00baee86a8134bc31bcc4f3b19abd9161ae
Rosa <rosarenee@att.net>
2021-07-20 18:49:21 -0500

    commit tree 5ef71136a8099a7403c843cba92e5652db6d1b95

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { unstable_renderSubtreeIntoContainer } from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const NewHeader = <h1>now</h1>
+
+ReactDOM.render(<App />, 
+    document.getElementById("root"));
+
